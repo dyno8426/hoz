720305b2bf5b673e3a57fb8cb051ec2004285b7c
Adarsh Chauhan <adarshc@Adarshs-MacBook-Pro.local>
2015-11-03 03:00:23 +0530

    created Environment.hs

---
+++ Environment.hs
@@ -0,0 +1,24 @@
+module Environment where
+
+import Data.Map
+import qualified Data.Map as Map
+import SAS
+
+type Env = Map String Int
+
+initializeEnv :: Env
+initializeEnv = Map.empty
+
+-- returns the SAS location of the variable
+getVarInEnv :: String -> Env -> Int
+getVarInEnv key env = case Map.lookup key env of
+							Just a -> a
+							Nothing -> -1
+
+checkIfVarInEnv :: String -> Env -> Bool
+checkIfVarInEnv key env = Map.member key env
+
+mergeLocalEnv :: SAS -> Env -> String -> Env
+mergeLocalEnv sas parent_env var = Map.insert var (size new_sas) parent_env where
+									new_sas = Map.insert (size sas) "NULL" sas
+
--- SAS.hs
+++ SAS.hs
@@ -1,26 +1,36 @@
+module SAS where
+
 import Data.Map
 import qualified Data.Map as Map
 
-initializeSAS :: Map Int String
+type SAS = Map Int String
+
+initializeSAS :: SAS
 initializeSAS = Map.empty
 
-addKeyToSAS :: Int -> Map Int String -> Map Int String
+addKeyToSAS :: Int -> SAS -> SAS
 addKeyToSAS key sas = Map.insert key "NULL" sas
 
-retrieveFromSAS :: Int -> Map Int String -> String
+retrieveFromSAS :: Int -> SAS -> String
 retrieveFromSAS key sas = case Map.lookup key sas of
 							Just a -> a
 							Nothing -> "NULL"
 
-bindValToKeyInSAS :: Int -> String -> Map Int String -> Map Int String
+bindValToKeyInSAS :: Int -> String -> SAS -> SAS
 bindValToKeyInSAS key value sas = Map.insert key value sas
 
-sas = fromList([(1,"abc"),(2,"def")])
-new_sas = addKeyToSAS 3 sas
-str = retrieveFromSAS 3 new_sas
+displaySAS :: SAS -> Int -> Int -> String -> String
+displaySAS sas val n temp = if val==n
+							then temp
+							else displaySAS sas (val+1) n (temp ++ "(" ++ (show val) ++ "," ++ (retrieveFromSAS val sas) ++ ")")
 
-displaySAS
+--sas = fromList([(0,"abc"),(1,"def")])
+--new_sas = addKeyToSAS (size sas) sas
+--str = retrieveFromSAS 2 new_sas
+--sas_str = displaySAS new_sas 0 (size new_sas) ""
 
-main = do
-	putStrLn str
-	putStrLn "Hello world!"
+--main = do
+--	putStrLn str
+--	putStrLn sas_str
+--	putStrLn "Hello world!"
+
