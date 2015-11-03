eb61bc949db99ef64931844a281ed01540b56a2c
Adarsh Chauhan <adarshc@Adarshs-MacBook-Pro.local>
2015-11-04 03:30:04 +0530

    added "Main.hs"

---
+++ Main.hs
@@ -0,0 +1,18 @@
+module Main where
+	import SAS
+	import Environment
+
+	data Ident = Ident String deriving (Show, Eq)
+	data Value = Value String deriving (Show, Eq)
+	data Statement = Statement [PTree] deriving (Show, Eq)
+
+	data PTree = Nop
+				| LocalVar Ident Statement
+				| BindVarToVar Ident Ident
+				| BindVarToVal Ident Value
+				| Conditional Ident Statement Statement
+				| BindVarToProc Ident Ident Statement
+				| Apply Ident Ident String
+				deriving (Show, Eq)
+
+	
--- SAS.hs
+++ SAS.hs
@@ -1,36 +1,36 @@
 module SAS where
+	import Data.Map
+	import qualified Data.Map as Map
 
-import Data.Map
-import qualified Data.Map as Map
+	type SAS = Map Int String
 
-type SAS = Map Int String
+	initializeSAS :: SAS
+	initializeSAS = Map.empty
 
-initializeSAS :: SAS
-initializeSAS = Map.empty
+	addKeyToSAS :: Int -> SAS -> SAS
+	addKeyToSAS key sas = Map.insert key "NULL" sas
 
-addKeyToSAS :: Int -> SAS -> SAS
-addKeyToSAS key sas = Map.insert key "NULL" sas
+	retrieveFromSAS :: Int -> SAS -> String
+	retrieveFromSAS key sas = case Map.lookup key sas of
+								Just a -> a
+								Nothing -> "NULL"
 
-retrieveFromSAS :: Int -> SAS -> String
-retrieveFromSAS key sas = case Map.lookup key sas of
-							Just a -> a
-							Nothing -> "NULL"
+	bindValToKeyInSAS :: Int -> String -> SAS -> SAS
+	bindValToKeyInSAS key value sas = Map.insert key value sas
 
-bindValToKeyInSAS :: Int -> String -> SAS -> SAS
-bindValToKeyInSAS key value sas = Map.insert key value sas
+	displaySAS :: SAS -> Int -> Int -> String -> String
+	displaySAS sas val n temp = if val==n
+								then temp
+								else displaySAS sas (val+1) n (temp ++ "(" ++ (show val) ++ "," ++ (retrieveFromSAS val sas) ++ ")")
 
-displaySAS :: SAS -> Int -> Int -> String -> String
-displaySAS sas val n temp = if val==n
-							then temp
-							else displaySAS sas (val+1) n (temp ++ "(" ++ (show val) ++ "," ++ (retrieveFromSAS val sas) ++ ")")
+	--sas = fromList([(0,"abc"),(1,"def")])
+	--new_sas = addKeyToSAS (size sas) sas
+	--str = retrieveFromSAS 2 new_sas
+	--sas_str = displaySAS new_sas 0 (size new_sas) ""
 
---sas = fromList([(0,"abc"),(1,"def")])
---new_sas = addKeyToSAS (size sas) sas
---str = retrieveFromSAS 2 new_sas
---sas_str = displaySAS new_sas 0 (size new_sas) ""
+	--main = do
+	--	putStrLn str
+	--	putStrLn sas_str
+	--	putStrLn "Hello world!"
 
---main = do
---	putStrLn str
---	putStrLn sas_str
---	putStrLn "Hello world!"
-
+	
