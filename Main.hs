d9004345a4d61092bfc21d42222e73d49a0a0b9c
Adarsh Chauhan <adarshc@Adarshs-MacBook-Pro.local>
2015-11-04 14:48:52 +0530

    added initial semnatic stack implementation

--- Main.hs
+++ Main.hs
@@ -3,16 +3,37 @@ import Environment
 
 data Ident = Ident String deriving (Show, Eq)
 data Value = Value String deriving (Show, Eq)
-data Statement = Statement [PTree] deriving (Show, Eq)
+data Operand = Plus | Minus | Multiply | Divide deriving (Show, Eq)
+--data Statements = Statements [PTree] deriving (Show, Eq)
+
+type Statements = [PTree]
+type SemanticStack = [(PTree,Env)]
 
 data PTree = Nop
-			| LocalVar Ident Statement
+			| LocalVar Ident Statements
 			| BindVarToVar Ident Ident
 			| BindVarToVal Ident Value
-			| Conditional Ident Statement Statement
-			| BindVarToProc Ident Ident Statement
+			| Conditional Ident Statements Statements
+			| BindVarToProc Ident Ident Statements
 			| Apply Ident Ident String
+			| OperateWithVar Ident Ident Operand Ident
+			| OperateWithVal Ident Ident Operand Value
 			deriving (Show, Eq)
 
+pushToSemanticStack :: SemanticStack -> Statements -> Env -> SemanticStack
+pushToSemanticStack semstack [] _ = semstack
+pushToSemanticStack semstack (stmt:stmts) env = pushToSemanticStack ((stmt,env):semstack) stmts env
+
+executeProgram :: SemanticStack -> SAS -> String
+executeProgram semstack sas = case semstack of
+	[] -> show sas
+	top:bottom -> show semstack
+
+program = [LocalVar (Ident "x") [BindVarToVal (Ident "x") (Value "1")],LocalVar (Ident "y") [BindVarToVal (Ident "y") (Value "2")]]
+sas = SAS.initializeSAS
+env = Environment.initializeEnv
+semstack = pushToSemanticStack [] (reverse program) env
+
 main = do
-		putStrLn "Hello world!"
+	putStrLn $ executeProgram semstack sas
+	putStrLn "Hello world!"
