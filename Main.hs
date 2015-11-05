5efcedf2ccd23e5b22a419ec0243a5229736f6ed
Adarsh Chauhan <adarshc@Adarshs-MacBook-Pro.local>
2015-11-05 20:13:24 +0530

    improved EqSets implementation and added BindVarToVar statement's functionality

--- Main.hs
+++ Main.hs
@@ -56,14 +56,19 @@ executeStatement sem_stack sas eq_sets = case curr_stmt of
 		(new_env,new_sas,new_eq_sets) = Environment.mergeLocalEnv sas eq_sets curr_env x
 		popped_stack = tail sem_stack
 		new_sem_stack = pushToSemanticStack popped_stack (reverse stmts) new_env
-	BindVarToVal (Ident x) (Value y) -> (new_sem_stack,new_sas,eq_sets) where
+	BindVarToVal (Ident x) (Value v) -> (new_sem_stack,new_sas,eq_sets) where
 		-- TO DO: INCLUDE ERROR CHECKING - IF THE GIVEN IDENTIFIER (x) IS DECLARED OR NOT
 		new_sem_stack = tail sem_stack
-		new_sas = SAS.bindValToKeyInSAS key y sas eq_sets where
+		new_sas = SAS.bindValToKeyInSAS key v sas eq_sets where
 			key = Environment.getVarInEnv x curr_env
 			--if Environment.checkIfVarInEnv x curr_env
 			--	then key = Environment.getVarInEnv x curr_env
 			--	else error x ++ " not declared"
+	BindVarToVar (Ident x) (Ident y) -> (new_sem_stack,new_sas,new_eq_sets) where
+		new_sem_stack = tail sem_stack
+		(new_sas,new_eq_sets) = SAS.bindKeyToKeyInSAS key_x key_y sas eq_sets where
+			key_x = Environment.getVarInEnv x curr_env
+			key_y = Environment.getVarInEnv y curr_env
 	Conditional (Ident x) stmts_if stmts_else -> selectStatements val stmts_if stmts_else sem_stack sas eq_sets curr_env where
 		key = Environment.getVarInEnv x curr_env
 		val = SAS.retrieveFromSAS key sas
@@ -74,13 +79,22 @@ executeStatement sem_stack sas eq_sets = case curr_stmt of
 			val = applyArithmeticOperation op val_x v where
 				key_x = Environment.getVarInEnv x curr_env
 				val_x = SAS.retrieveFromSAS key_x sas
+	OperateWithVar (Ident r) (Ident x) op (Ident y) -> (new_sem_stack,new_sas,eq_sets) where
+		new_sem_stack = tail sem_stack
+		new_sas = SAS.bindValToKeyInSAS key val sas eq_sets where
+			key = Environment.getVarInEnv r curr_env
+			val = applyArithmeticOperation op val_x val_y where
+				key_x = Environment.getVarInEnv x curr_env
+				val_x = SAS.retrieveFromSAS key_x sas
+				key_y = Environment.getVarInEnv y curr_env
+				val_y = SAS.retrieveFromSAS key_y sas
 	where
 		curr_stmt = fst $ popFromSemanticStack sem_stack
 		curr_env = snd $ popFromSemanticStack sem_stack
 
 executeProgram :: SemanticStack -> SAS -> EqSets -> String
 executeProgram sem_stack sas eq_sets = case sem_stack of
-	[] -> show sas
+	[] -> concat ["SAS: ", show sas, "\n", "EqSets: ", show eq_sets]
 	top:bottom -> executeProgram new_sem_stack new_sas new_eq_sets where
 		(new_sem_stack,new_sas,new_eq_sets) = executeStatement sem_stack sas eq_sets
 
@@ -93,9 +107,12 @@ program_6 = [LocalVar (Ident "a") [LocalVar (Ident "a") [BindVarToVal (Ident "a"
 program_7 = [LocalVar (Ident "clause") [LocalVar (Ident "p") [BindVarToVal (Ident "clause") (Value "true"), Conditional (Ident "clause") [BindVarToVal (Ident "p") (Value "clause was true")] [BindVarToVal (Ident "p") (Value "clause was false")]]]]
 program_8 = [Nop, LocalVar (Ident "a") [BindVarToVal (Ident "a") (Value "1")], LocalVar (Ident "b") [BindVarToVal (Ident "b") (Value "2"), BindVarToVal (Ident "a") (Value "3")], Nop]
 program_9 = [LocalVar (Ident "result") [LocalVar (Ident "variable") [BindVarToVal (Ident "variable") (Value "40"), OperateWithVal (Ident "result") (Ident "variable") Plus (Value "2")]]]
+program_10 = [LocalVar (Ident "d") [LocalVar (Ident "e") [LocalVar (Ident "f") [BindVarToVal (Ident "e") (Value "5"), BindVarToVal (Ident "f") (Value "6"), OperateWithVar (Ident "d") (Ident "e") Plus (Ident "f")]]]]
+program_11 = [LocalVar (Ident "p") [LocalVar (Ident "q") [BindVarToVar (Ident "p") (Ident "q"), LocalVar (Ident "r") [BindVarToVar (Ident "r") (Ident "p"), BindVarToVal (Ident "q") (Value "42")]]]]
+program_12 = [LocalVar (Ident "m") [BindVarToVal (Ident "m") (Value "true"), LocalVar (Ident "n") [BindVarToVal (Ident "n") (Value "false"), LocalVar (Ident "o") [BindVarToVar (Ident "m") (Ident "n")]]]]
 (sas,eq_sets) = SAS.initializeSAS
 env = Environment.initializeEnv
-sem_stack = pushToSemanticStack [] (reverse program_7) env
+sem_stack = pushToSemanticStack [] (reverse program_11) env
 
 main = do
 	putStrLn $ executeProgram sem_stack sas eq_sets
--- SAS.hs
+++ SAS.hs
@@ -36,6 +36,32 @@ module SAS where
 	bindValToKeyInSAS key value sas eq_sets = insertIntoAllEqSetKeys all_keys value sas where
 													all_keys = retrieveFromEqSets key eq_sets
 
+	--mergeEqSets :: EqSets -> Int -> Int -> EqSets
+	--mergeEqSets eq_sets key_x key_y = new_eq_sets where
+	--	temp_eq_sets = Map.insert (key_x) new_x_set eq_sets where
+	--		new_x_set = concat [(retrieveFromEqSets key_x eq_sets), (retrieveFromEqSets key_y eq_sets)]
+	--	new_eq_sets = Map.insert (key_y) new_y_set temp_eq_sets where
+	--		new_y_set = concat [(retrieveFromEqSets key_y eq_sets), (retrieveFromEqSets key_x eq_sets)]
+	mergeEqSetsRecursively :: EqSets -> [Int] -> [Int] -> EqSets
+	mergeEqSetsRecursively eq_sets _ [] = eq_sets
+	mergeEqSetsRecursively eq_sets merged_eq_set (key:keys) = mergeEqSetsRecursively (Map.insert key merged_eq_set eq_sets) merged_eq_set keys
+
+	findMergedEqSet :: EqSets -> Int -> Int -> [Int]
+	findMergedEqSet eq_sets key_x key_y = concat [(retrieveFromEqSets key_x eq_sets), (retrieveFromEqSets key_y eq_sets)]
+
+	bindKeyToKeyInSAS :: Int -> Int -> SAS -> EqSets -> (SAS,EqSets)
+	bindKeyToKeyInSAS key_x key_y sas eq_sets = if (retrieveFromSAS key_x sas)=="NULL"
+												then	if (retrieveFromSAS key_y sas)=="NULL"
+														--then (sas,(mergeEqSets eq_sets key_x key_y))
+														--else ((bindValToKeyInSAS key_x (retrieveFromSAS key_y sas) sas eq_sets),(mergeEqSets eq_sets key_x key_y))
+														then (sas,(mergeEqSetsRecursively eq_sets (findMergedEqSet eq_sets key_x key_y) (findMergedEqSet eq_sets key_x key_y)))
+														else ((bindValToKeyInSAS key_x (retrieveFromSAS key_y sas) sas eq_sets),(mergeEqSetsRecursively eq_sets (findMergedEqSet eq_sets key_x key_y) (findMergedEqSet eq_sets key_x key_y)))
+												else	if (retrieveFromSAS key_y sas)=="NULL"
+														then ((bindValToKeyInSAS key_y (retrieveFromSAS key_x sas) sas eq_sets),(mergeEqSetsRecursively eq_sets (findMergedEqSet eq_sets key_x key_y) (findMergedEqSet eq_sets key_x key_y)))
+														else	if (retrieveFromSAS key_x sas)==(retrieveFromSAS key_y sas)
+																then (sas,(mergeEqSetsRecursively eq_sets (findMergedEqSet eq_sets key_x key_y) (findMergedEqSet eq_sets key_x key_y)))
+																else error "Binding incompatible values in SAS."
+
 	displaySAS :: SAS -> String
 	displaySAS = show
 
